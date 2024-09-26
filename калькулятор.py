c34121360b7c80e1530eb5eb91cc90626d5e6a88
К-Эндрю-Ю <kozmaprudkov@gmail.com>
2024-09-26 16:19:39 +0300

    добавлена функция деление

--- калькулятор.py
+++ калькулятор.py
@@ -4,8 +4,9 @@ def subtruct (x,y):#subtruct - вычитание
     return x-y
 def multiply (a,b): #multiply - умножение
     return a*b
-
+def divide (x,y): # divide - деление
 
 print(add (3,4))
 print(subtruct (10,4))
 print(multiply (2,4))
+print(divide (10,2))
