b067247c7499993e627624c78688e83c868ec4d6
К-Эндрю-Ю <kozmaprudkov@gmail.com>
2024-09-26 16:03:52 +0300

    добавлена функция умножения

--- калькулятор.py
+++ калькулятор.py
@@ -2,6 +2,10 @@ def add (x,y): # add - сложение
     return x+y # return - возвращает
 def subtruct (x,y):#subtruct - вычитание
     return x-y
+def multiply (a,b): #multiply - умножение
+    return a*b
+
 
 print(add (3,4))
 print(subtruct (10,4))
+print(multiply (2,4))
